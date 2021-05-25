03022a2c6a559d03f2128538315ed064230d8d26
v1nni7 <vinicezar800@hotmail.com>
2021-05-25 17:06:04 -0300

    Listagem de filmes

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
 import React from 'react';
+import Movie from './screens/ListMovie'
 
-export default class App extends React.Component {
-    dummy = () => {
-        // Just to test arrow functions
-    }
+const App = () => {
+    return (
+        <Movie />
+    );
+};
 
-    render() {
-        return (
-            <div className={'hello'}>
-                <h2>Hello Electrate</h2>
-                <img src="./assets/logo.png" />
-                <h4>A basic Electron + React.js template</h4>
-                <h4>Have Fun!</h4>
-            </div>
-        );
-    }
-}
+export default App;
---
+++ src/components/Movie/Movies.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Movies = ({ movie }) => {
+    return (
+        <>
+            <div>
+                <h3>{movie.nome}</h3>
+            </div>
+        </>
+    );
+};
+
+export default Movies;
