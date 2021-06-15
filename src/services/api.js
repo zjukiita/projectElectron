5e81dbda6e6e226e5868f760f2ed9c5e4feb453f
ricesar21 <ricesar21@gmail.com>
2021-06-15 18:21:03 -0300

    nova API

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://senacmobile.herokuapp.com',
+    baseURL: 'https://senacretro.herokuapp.com',
 });
 
 export default api;
